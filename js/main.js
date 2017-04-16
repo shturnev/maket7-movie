e6eaa0b177aecdc6729054115f507f6dd4b52b57
shturnev <sht_job@ukr.net>
2017-04-16 14:03:04 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('ul.slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    appendDots: $(".forDots")
+});
+
+$('[data-js="open-full-info"]').on("click", function () {
+
+    $(this).parents('.wr').find('.full-info').fadeToggle('fast');
+
+    return false;
+});
+
+
+$('[data-js="close-full-info"]').on("click", function () {
+
+    $(this).parents('.full-info').fadeOut('fast');
+    
+    return false;
+});
+
